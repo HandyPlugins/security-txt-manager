4c22073a9d27433cc24d72bf1ab5e76eb7619e6e
mustafauysal <me@uysalmustafa.com>
2026-05-31 22:01:52 +0300

    fix plugin check

--- includes/constants.php
+++ includes/constants.php
@@ -7,5 +7,9 @@
 
 namespace SecuritytxtManager\Constants;
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
 const CAPABILITY     = 'manage_security_txt';
 const SETTING_OPTION = 'security_txt_manager';
