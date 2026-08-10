6ec629fa8f4ed330c206cd10153ece4bae157999
mustafauysal <me@uysalmustafa.com>
2026-08-10 23:00:32 +0300

    Prevent canonical redirects for security.txt endpoints

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * PHPUnit bootstrap file.
+ *
+ * @package SecuritytxtManager
+ */
+
+define( 'ABSPATH', dirname( __DIR__ ) . '/' );
+
+require_once dirname( __DIR__ ) . '/includes/constants.php';
+require_once dirname( __DIR__ ) . '/includes/core.php';
